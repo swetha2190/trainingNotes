79d9378a404e8687e79b814e54bbe149c779568f
swetha2190 <43073957+swetha2190@users.noreply.github.com>
2018-12-21 10:07:09 +0530

    Add files via upload

---
+++ TableLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3
+{
+    class TableLogic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num2; i++)
+            {
+                Console.WriteLine(num1+ "* " + i + " =" + (num1* i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
