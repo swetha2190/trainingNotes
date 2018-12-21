e2cf8a43d68461c4fdb7f6158849896a43521c3c
swetha2190 <43073957+swetha2190@users.noreply.github.com>
2018-12-21 10:06:48 +0530

    Add files via upload

---
+++ Vowelwala.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3
+{
+    class Vowelwala
+    {
+
+        public void Display()
+        {
+            Console.WriteLine("enter a character ::");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            switch(ch)
+            {
+                case 'a':
+                case 'A':
+                    
+                case 'e':
+                case 'E':
+                case 'i':
+                case 'I':
+                case 'o':
+                case 'O':
+                case 'u':
+                case 'U':
+                Console.WriteLine("it is vowel");break;
+                default:
+
+                    Console.WriteLine("it is not vowel"); break;
+
+
+
+            }
+        }
+    }
+
+    class VowelRunner
+    {
+        static void Main(string[] args)
+        {
+            Vowelwala v = new Vowelwala();
+            v.Display();
+            Console.ReadLine();
+        }
+    }
+}
